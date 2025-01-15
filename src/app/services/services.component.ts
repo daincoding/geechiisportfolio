12ae8cbdbf61ec0aa1060811662d16a4a4de9048
daincoding <official.toyou@gmail.com>
2025-01-15 10:13:27 +0100

    Style the Services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  services = ["Commissions are currently: CLOSED", "VGEN COMM: CLOSED", "Waitlist: CLOSED", "TrelloBoard"];
 
+  getRandomRotation() {
+    let randomVal = (Math.random() * 30) - 15;
+    return `rotate(${randomVal}deg)`;
+  }
 }
