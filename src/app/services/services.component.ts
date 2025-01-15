c9aa2e2bf9c2ffe055aed542f35bb0b4c3433198
daincoding <official.toyou@gmail.com>
2025-01-15 11:46:16 +0100

    Service Dragging and New Save Component

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
@@ -8,11 +8,39 @@ import { Component } from '@angular/core';
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
   services = ["Commissions are currently: CLOSED", "VGEN COMM: CLOSED", "Waitlist: CLOSED", "TrelloBoard"];
+  servicePosition: string[][] = [];
+  currentlyDragging : number | undefined;
+  mouseX : number | undefined;
+  mouseY : number | undefined;
+
+  ngOnInit() {
+      for (let i = 0 ; i < this.services.length; i++) {
+        let pos: string[] = [i * (90 / this.services.length) + '%', ((i + 1) % 3) * 33 + '%', this.getRandomRotation() ]
+        this.servicePosition.push(pos);
+      }
+  }
 
   getRandomRotation() {
     let randomVal = (Math.random() * 30) - 15;
     return `rotate(${randomVal}deg)`;
   }
+
+  startDragging(i: number) {
+    this.currentlyDragging = i;
+  }
+
+  mouseMove(e: MouseEvent) {
+    if (this.currentlyDragging == undefined) { return; }
+
+    this.servicePosition [this.currentlyDragging] [0] = e.pageX - 70 + 'px'
+    this.servicePosition[this.currentlyDragging] [1] = e.pageY - 30 + 'px'
+  }
+
+  stopDragging(){
+    this.currentlyDragging = undefined;
+  }
 }
+
+  
